947894f27425f30b562473ce1600c60e77e4e482
Jordan Miguel <jordan.m.forde@gmail.com>
2017-08-16 03:25:48 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+// set- up application part 🙌🏾
+(function)() {
+    ....
+
+})();
+
+// set- up another application part 🙌🏾
+(function)() {
+    ....
+
+})();
+
+
+// set- up different application part 🙌🏾
+(function)() {
+    ....
+
+})();
+
+
+// set- up another application part 🙌🏾
+(function)() {
+    ....
+
+})();
+
+
+// Here we are creating seperate self calling closures for independent pieces of 
+// functionality.
+// Essentially, each closure can be thought of as an 'individual' module that adds
+// some type of functionality to the application without being reliant on anything
+// outside it (which we don't want for obvious reasons).
+
+// This keeps the global namespace just about untouched.
